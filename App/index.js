cc290d126f056608bc04f561a31fd9d9d53338c3
Jonas_Mateus <jonasmateusbezerradossantos@gmail.com>
2020-10-11 19:30:31 -0300

    Refatoração de código, conserto de bug do título e font.

--- App/index.js
+++ App/index.js
@@ -1,55 +1,40 @@
 
-
     const typingArea = $('#editor')
     const editorContext = typingArea[0].contentDocument
     editorContext.designMode = 'on'
-    
+    editorContext.body.style.fontFamily = "Arial, sans-serif"
+
+    const titleButton = $('body > div > div > div > div.text-titles-container > select')
+    const selectionTag = titleButton[0]
+    const boldButton = $('div.bold > button')
+    const underlineButton = $('div.underline > button')
+    const italicButton = $('div.italic > button')
+    const styledQuotesButton = $('div.quotes > button')
+    const stikeButton = $('div.strike > button')
+    const heightLight = $('div.highlight > button')
+    const fontColorButton = $('div.font-color > button')
+    const leftTextAlignButton =  $('div.left-text-align > button')
+    const rightTextAlignButton = $('div.right-text-align > button')
+    const centerTextAlignButton = $('div.center-text-align > button')
+    const justifiedTextButton = $('div.justify-text-align > button')
+    const numberedListButton = $('div.numbers-list > button')
+    const bulletListButton = $('div.lines-bullets > button')
+    const imageButton = $('div.add-img > button')
+    const linkButton = $('div.add-link > button')
+    const dowloadButton = $('div.download > button')
+
     $(editorContext).ready( function (){
 
-        const titleButton = $('body > div > div > div > div.text-titles-container > select')
-        const selectionTag = titleButton[0]
-        selectionTag.onchange = (event) => inspectUserSelection(event)
-        console.log(selectionTag)
-    
-        function inspectUserSelection (event) {
-            
-            const elementSign = event.target.value
-            const selectedPosition = editorContext.getSelection()
-            editorContext.onmouseup = ( ) => applyElementWithSign(elementSign, selectedPosition)
-            
-        }
+        enableStylingFeatures()
+        enableFormatingFeatures()
+        enableOrdeningFeatures()
+        enableExtraFeatures()
 
-        
-        function applyElementWithSign (sing,position) {
+    })
 
-            let element = document.createElement(`${sing}`)
-            let range = new Range()
-            range.setStart(position.anchorNode, position.anchorOffset)
-            range.setEnd(position.focusNode, position.focusOffset)
-            range.surroundContents(element)
-            
-        }
+    function enableStylingFeatures() {
 
-        const boldButton = $('div.bold > button')
-        const underlineButton = $('div.underline > button')
-        const italicButton = $('div.italic > button')
-        const styledQuotesButton = $('div.quotes > button')
-        const stikeButton = $('div.strike > button')
-        const heightLight = $('div.highlight > button')
-        const fontColorButton = $('div.font-color > button')
-    
-        const leftTextAlignButton =  $('div.left-text-align > button')
-        const rightTextAlignButton = $('div.right-text-align > button')
-        const centerTextAlignButton = $('div.center-text-align > button')
-        const justifiedTextButton = $('div.justify-text-align > button')
-    
-        const numberedListButton = $('div.numbers-list > button')
-        const bulletListButton = $('div.lines-bullets > button')
-    
-        const imageButton = $('div.add-img > button')
-        const linkButton = $('div.add-link > button')
-        const dowloadButton = $('div.download > button')
-    
+        selectionTag.onchange = (event) => inspectUserSelection(event)    
         boldButton.click ( event => {  editorContext.execCommand('bold', false, null) } )
         underlineButton.click ( event => {  editorContext.execCommand('underline', false, null) } )
         italicButton.click ( event => { editorContext.execCommand('italic', false, null) } ) 
@@ -58,7 +43,6 @@
            
             editorContext.execCommand('formatBlock', false, '<blockquote>')
             editorContext.execCommand('italic', false, null)
-    
         } )
     
         stikeButton.click( event => { editorContext.execCommand('strikeThrough', false, null) } )
@@ -66,22 +50,54 @@
         heightLight.click( event => {
             
             const color = window.prompt('Type a color: ').toString()
-            editorContext.execCommand('hiliteColor', false, color) } )
+            editorContext.execCommand('hiliteColor', false, color) 
+        } )
     
         fontColorButton.click( event => {
     
             const fontColor = window.prompt('Type the font color: ').toString()
             editorContext.execCommand('forecolor', false, fontColor)
-            
         })
+
+    }
+
+    function inspectUserSelection (event) {
+            
+        const elementSign = event.target.value
+        const selectedPosition = editorContext.getSelection()
+        console.log(selectedPosition)
+        applyElementWithSign(elementSign, selectedPosition)
+        
+    }
     
+    function applyElementWithSign (sing,position) {
+
+        let element = document.createElement(`${sing}`)
+        let range = new Range()
+        range.setStart(position.anchorNode, position.anchorOffset)
+        range.setEnd(position.focusNode, position.focusOffset)
+        range.surroundContents(element)
+ 
+    }
+
+    function enableFormatingFeatures() {
+
         leftTextAlignButton.click( event => { editorContext.execCommand('justifyLeft', false) } )
         rightTextAlignButton.click( event => { editorContext.execCommand('justifyRight', false) } )
         centerTextAlignButton.click( event => { editorContext.execCommand('justifyCenter', false) } )
         justifiedTextButton.click( event => { editorContext.execCommand('justifyFull', false, null) } )
-    
+
+    }
+
+    function enableOrdeningFeatures() {
+
         numberedListButton.click( event => { editorContext.execCommand('insertOrderedList', false) } )
         bulletListButton.click( event => { editorContext.execCommand('insertUnorderedList', false) } )
+
+    }
+
+    function enableExtraFeatures() {
+
         linkButton.click( event => {
     
             const url = window.prompt('Type the URL: ').toString()
@@ -95,7 +111,7 @@
             editorContext.execCommand('insertImage', false, url)
     
          })
-    
-    })
 
+    }
+    
 
